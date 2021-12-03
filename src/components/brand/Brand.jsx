fafc82a5637d4def1b265481ab35d1143f27a407
rohitjoey <roht.praz@gmail.com>
2021-12-03 19:27:54 +0545

    Feature section done

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./brand.css";
+import { atlassian, dropbox, google, shopify, slack } from "./import";
 
 const Brand = () => {
-  return <div>brand</div>;
+  return (
+    <div className="gpt3__brand section__padding">
+      <div>
+        <img src={google} alt="google" />
+      </div>
+      <div>
+        <img src={slack} alt="slack" />
+      </div>
+      <div>
+        <img src={atlassian} alt="atlasian" />
+      </div>
+      <div>
+        <img src={dropbox} alt="dropbox" />
+      </div>
+      <div>
+        <img src={shopify} alt="shopify" />
+      </div>
+    </div>
+  );
 };
 
 export default Brand;
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -1,8 +1,44 @@
 import React from "react";
+import Feature from "./../../components/feature/Feature";
 import "./whatGPT3.css";
 
 const WhatGPT3 = () => {
-  return <div>WhatGPT3</div>;
+  const feature_list = [
+    {
+      title: "Chatbots",
+      text: "We so opinion friends me message as delight. Whole front do of plate heard oh ought. ",
+    },
+    {
+      title: "Knowledgebase",
+      text: "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b",
+    },
+    {
+      title: "Education",
+      text: "At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments b",
+    },
+  ];
+
+  return (
+    <div className="gpt3__whatgpt3 section__margin" id="whatgpt3">
+      <div className="gpt3__whatgpt3-feature">
+        <Feature
+          title="What is GPT-3"
+          text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by."
+        />
+      </div>
+      <div className="gpt3__whatgpt3-heading">
+        <h1 className="gradient__text">
+          The possibilities are beyond your imagination
+        </h1>
+        <p>Explore The Library</p>
+      </div>
+      <div className="gpt3__whatgpt3-container">
+        {feature_list.map((obj) => (
+          <Feature title={obj.title} text={obj.text} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default WhatGPT3;
