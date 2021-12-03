5bffba9d22b79c780896547222449b8f1ada6832
rohitjoey <roht.praz@gmail.com>
2021-12-03 23:50:39 +0545

    Footer done

--- src/components/article/Article.jsx
+++ src/components/article/Article.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import "./article.css";
 
-const Article = () => {
-  return <div>Article</div>;
+const Article = ({ imgUrl, title, date }) => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="articles" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+        <div>
+          <p>{date}</p>
+          <h3>{title}</h3>
+        </div>
+        <p>Read Full Article</p>
+      </div>
+    </div>
+  );
 };
 
 export default Article;
--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,8 +1,60 @@
 import React from "react";
+import { Article } from "../../components";
 import "./blog.css";
+import { blog01, blog02, blog03, blog04, blog05 } from "./imports.js";
+
+const blogList = [
+  {
+    imgUrl: blog02,
+    title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
+    date: "Sep 26, 2021",
+  },
+  {
+    imgUrl: blog03,
+    title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
+    date: "Sep 26, 2021",
+  },
+  {
+    imgUrl: blog04,
+    title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
+    date: "Sep 26, 2021",
+  },
+  {
+    imgUrl: blog05,
+    title: "GPT-3 and Open  AI is the future. Let us exlore how it is?",
+    date: "Sep 26, 2021",
+  },
+];
 
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className="gpt3__blog section__padding" id="blog">
+      <div className="gpt3__blog-heading">
+        <h1 className="gradient__text">
+          A lot is happening, We are blogging about it.
+        </h1>
+      </div>
+      <div className="gpt3__blog-container">
+        <div className="gpt3__blog-container_groupA">
+          <Article
+            imgUrl={blog01}
+            title="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+            date="Sep 26, 2021"
+          />
+        </div>
+        <div className="gpt3__blog-container_groupB">
+          {blogList.map((obj, index) => (
+            <Article
+              imgUrl={obj.imgUrl}
+              title={obj.title}
+              date={obj.date}
+              key={index}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Blog;
